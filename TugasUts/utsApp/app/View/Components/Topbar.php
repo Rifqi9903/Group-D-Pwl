033437ae3f18d020ab1609729d31c8cd8e5d50b7
fardz <faridzazzarqi39@gmail.com>
2023-11-03 18:51:28 +0700

    fix(topbar) memperbaiki component top bar [faridz]

--- TugasUts/utsApp/app/View/Components/Topbar.php
+++ TugasUts/utsApp/app/View/Components/Topbar.php
@@ -9,13 +9,15 @@ use Illuminate\View\Component;
 class Topbar extends Component
 {
     public $title;
+    public $slash;
     public $subTitle;
     /**
      * Create a new component instance.
      */
-    public function __construct($title=null, $subTitle=null)
+    public function __construct($title, $slash=null, $subTitle=null)
     {
         $this->title=$title;
+        $this->slash=$slash;
         $this->subTitle=$subTitle;
     }
 
